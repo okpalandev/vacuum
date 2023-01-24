9531448223c1382bf8ad038377f857f5702c796a
Nnamdi Michael Okpala <codewithnnamei@proton.me>
2023-01-24 23:10:29 +0000

    a

--- lib/main.lua
+++ lib/main.lua
@@ -1,6 +1,4 @@
-package.path = package.path .. ';' .. arg[0]:gsub('[^\\/]+$', '') .. '?.lua'  -- add current directory to package path
 -- export all the modules in this directory
-
 return {
     matrix = require('.matrix'),
     vector = require('.vector') ,
--- main.lua
+++ main.lua
@@ -1,6 +1,1 @@
-require(".lib.main");
-
-x0 = vector.vec2:new(1, 2);
-y0 = vector.vec2:new(3, 4);
-z0 = x0 + y0;
-print(z0);
+package.path = package.path .. ';' .. arg[0]:gsub('[^\\/]+$', '') .. 'lib.?.lua'  -- add current directory to package path
